48a29c9e73b6865783d1a9aef88fdc98310ad55b
kenkool <kenkool@greeny.cs.tlu.ee>
2016-09-08 13:11:07 +0300

    commitAdded my first line of code

---
+++ first_file.php
@@ -0,0 +1,13 @@
+<?php
+$firstName = "Ken";
+$lastName = "Koolmeister";
+
+// echo $firstName;
+// echo $lastName;
+
+// Kahe stringi liitmiseks kasutan punkti
+echo $firstName." " .$lastName
+
+
+?>
+
